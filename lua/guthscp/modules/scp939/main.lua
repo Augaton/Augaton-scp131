6eb9b5ad5250545ca2581adc5f744d5fc26a5909
Augaton <Mulleraurelien@hotmail.fr>
2025-12-19 11:24:29 +0100

    [Change] Require 173 (duh)

--- lua/guthscp/modules/scp939/main.lua
+++ lua/guthscp/modules/scp939/main.lua
@@ -7,6 +7,7 @@ local MODULE = {
 	version_url = "https://raw.githubusercontent.com/Augaton/guthscp939/refs/heads/main/lua/guthscp/modules/scp939/main.lua",
 	dependencies = {
 		base = "2.4.0",
+		guthscp173 = "2.0.4"
         guthscpkeycard = "optional:2.1.6",
 	},
 	requires = {
